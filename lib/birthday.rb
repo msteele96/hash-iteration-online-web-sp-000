3b0643384a5238f2b53c9a64170e9e9c3ae4683b
Marc Steele <marcsteele2014@gmail.com>
2020-10-11 18:58:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,12 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |name, kid|
+    if age < 18 
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else
+      puts "You shouldn't be having a birthday party at Chuck E. Cheese's."
+  end
+end
